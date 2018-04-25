7f78a92a61c9f81daa1c8b3cbec0cdda8e8852c9
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2018-04-25 15:32:58 -0400

    Analysis script

---
+++ src/frontend/analyze.js
@@ -0,0 +1,22 @@
+import {analyzeLog} from './src/Analyzer.js';
+var fs = require('fs');
+
+console.log = console.warn;
+global.alert = () => {};
+
+// see https://gist.github.com/kristopherjohnson/5065599
+function readStdin(callback) {
+  var data = '';
+  var stdin = process.stdin;
+  stdin.resume();
+  stdin.setEncoding('utf8');
+  stdin.on('data', function (chunk) { data += chunk; });
+  stdin.on('end', function() {
+    callback(null, data);
+  });
+}
+
+readStdin(function(err, res) {
+  var log = res.split('\n').filter(line => line.length > 0).map(line => JSON.parse(line));
+  analyzeLog(log).then(result => {process.stdout.write(JSON.stringify(result))});
+});
