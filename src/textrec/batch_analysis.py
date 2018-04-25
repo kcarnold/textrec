a1d2a51e8fe8c4991f98208efe5bf13f34dee7cb
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2018-04-25 17:29:44 -0400

    Quick summary of a batch.

--- src/textrec/batch_analysis.py
+++ src/textrec/batch_analysis.py
@@ -10,3 +10,19 @@ def get_participants_by_batch():
             participants = participants.strip().split()
             participants_by_batch[batch_name] = participants
     return participants_by_batch
+
+
+def summarize(batch):
+    participants = get_participants_by_batch()[batch]
+    for pid in participants:
+        print()
+        print(pid)
+        analyzed = analysis_util.get_log_analysis(pid)
+
+        for name, page in analyzed['byExpPage'].items():
+            print(':'.join((name, page['condition'], page['finalText'])))
+
+        for k, v in analyzed['allControlledInputs']:
+            if not isinstance(v, str):
+                continue
+            print(f'{k}: {v}')
