fb85c1730914a5738c97c0277379b2f27889875a
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2018-05-01 22:15:41 -0400

    Make a quick test for the controlledInputs behavior.

---
+++ src/frontend/src/IOTaskState.test.js
@@ -0,0 +1,18 @@
+import * as IOTaskState from "./IOTaskState";
+
+it("boots", () => {
+  let clientId = 'abc123';
+  let screens = [];
+  let handleEvent = () => {};
+
+  let state = new IOTaskState.MasterStateStore({
+    clientId,
+    screens,
+    handleEvent,
+    timeEstimate: "20-25 minutes",
+  });
+
+  state.handleEvent({type: 'controlledInputChanged', name: 'test', value: 123});
+
+  expect([...state.controlledInputs]).toEqual([['test', 123]]);
+});
