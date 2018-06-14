acf25625d26101a849a3a9dee9feb1f880c3f3b9
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2018-06-14 11:59:48 -0400

    Fix counterbalancing: count old but completed participants

--- src/textrec/counterbalancing.py
+++ src/textrec/counterbalancing.py
@@ -18,9 +18,7 @@ def completed_fname(participant_id, logdir):
     return logdir / f'{participant_id}.completed'
 
 
-def get_completion_data(batch, max_age_secs=12 * SECS_PER_HOUR, logdir=paths.logdir):
-    now = time.time()
-    oldest_timestamp = now - max_age_secs
+def get_completion_data(batch, logdir=paths.logdir):
     results = []
     for log_file in logdir.glob('*.jsonl'):
         try:
@@ -36,12 +34,10 @@ def get_completion_data(batch, max_age_secs=12 * SECS_PER_HOUR, logdir=paths.log
             continue
         if login_event.get('batch') != batch:
             continue
-        if login_event['pyTimestamp'] < oldest_timestamp:
-            print("Skipping old", log_file)
-            continue
         participant_id = login_event['participant_id']
         results.append(dict(
             participant_id=participant_id,
+            login_timestamp=login_event['pyTimestamp'],
             assignment=login_event['assignment'],
             completed=completed_fname(participant_id, logdir).exists()))
     return results
@@ -50,15 +46,30 @@ def get_completion_data(batch, max_age_secs=12 * SECS_PER_HOUR, logdir=paths.log
 def mark_completed(participant_id, logdir=paths.logdir):
     completed_fname(participant_id, logdir).touch()
 
+def get_expected_completions(num_conditions, completion_data, max_age_secs=12 * SECS_PER_HOUR):
+    now = time.time()
+    oldest_timestamp = now - max_age_secs
+    expected_completions = np.zeros(num_conditions)
+    for completion in completion_data:
+        assignment = completion['assignment']
+        if completion['completed']:
+            expected_completions[assignment] += 1.0
+        else:
+            age_secs = now - completion['login_timestamp']
+            if age_secs < max_age_secs:
+                # They might complete.
+                expected_completions[assignment] += 0.5
+            else:
+                print(f"Participant {completion['participant_id']} started too long ago ({age_secs/SECS_PER_HOUR:.1f}hr)")
+    return expected_completions
+
 
 def get_conditions_for_new_participant(batch):
     batch_data = BATCH_DATA[batch]
     completion_data = get_completion_data(batch)
-    expected_completions = np.zeros(batch_data['n_conditions'])
-    print(completion_data)
-    for completion in completion_data:
-        indicator = 1.0 if completion['completed'] else 0.5
-        expected_completions[completion['assignment']] += indicator
+    expected_completions = get_expected_completions(
+        batch_data['n_conditions'],
+        completion_data)
     print(expected_completions)
     assignment = int(np.argmin(expected_completions))
     return dict(batch_data, assignment=assignment)
