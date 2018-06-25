9f94362f529974a7b905c7e52b59b834dab1b35a
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2018-06-25 10:07:04 -0400

    Sanity checks for batch retrieval

--- src/textrec/batch_analysis.py
+++ src/textrec/batch_analysis.py
@@ -106,10 +106,12 @@ def get_participants_by_batch():
     participants_by_batch = {}
     with open(paths.data / 'participants.txt') as f:
         for line in f:
-            if line.startswith('#'):
+            line = line.strip()
+            if not line or line.startswith('#'):
                 continue
-            batch_name, participants = line.strip().split(':', 1)
+            batch_name, participants = line.split(':', 1)
             participants = participants.strip().split()
+            assert len(participants) == len(set(participants))
             participants_by_batch[batch_name] = participants
     return participants_by_batch
 
