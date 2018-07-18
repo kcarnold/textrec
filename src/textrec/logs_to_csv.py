11c76c4080dad4454a4b4856da41b301655a007d
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2018-07-18 10:25:57 -0400

    Move quick summary to separate file to use in notebooks

--- src/textrec/logs_to_csv.py
+++ src/textrec/logs_to_csv.py
@@ -111,45 +111,6 @@ def get_participants_by_batch():
     return participants_by_batch
 
 
-def summarize(participants, incomplete_ok=False):
-    for participant_id in participants:
-        print()
-        print(participant_id)
-        analyzed = analysis_util.get_log_analysis(participant_id)
-
-        if not incomplete_ok:
-            assert analyzed['isComplete'], f'INCOMPLETE! {participant_id}'
-
-        controlledInputsDict = dict(analyzed['allControlledInputs'])
-        if controlledInputsDict.get('shouldExclude', "No") == "Yes":
-            print("****** EXCLUDE! **********")
-            assert False
-
-        for name, page in analyzed['byExpPage'].items():
-            print(':'.join((name, page['condition'], page['finalText'])))
-
-        print()
-
-        for k, v in analyzed['allControlledInputs']:
-            if not isinstance(v, str):
-                continue
-            print(f'{k}: {v}')
-
-        screen_times = [
-            (s1['name'], (s2['timestamp'] - s1['timestamp']) / 1000)
-            for s1, s2 in toolz.sliding_window(2, analyzed['screenTimes'])
-            ]
-        c = Counter()
-        for name, secs in screen_times:
-            c[name] += secs
-
-        total_time = (
-            analyzed['screenTimes'][-1]['timestamp']
-             - analyzed['screenTimes'][0]['timestamp']) / 1000 / 60
-        print(f"\nTotal time: {total_time:.1f}m")
-        print('\n'.join('{}: {:.1f}'.format(name, secs) for name, secs in c.most_common()))
-
-
 def count_actions(actions):
     action_counts = dict(
         Counter(toolz.pluck('annoType', actions)))
