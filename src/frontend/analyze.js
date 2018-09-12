c6b58a71baab0bdb536af5d693926380ca1e204f
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2018-09-12 17:39:57 -0400

    Batch analyses.

--- src/frontend/analyze.js
+++ src/frontend/analyze.js
@@ -4,22 +4,30 @@ var fs = require('fs');
 console.log = console.warn;
 global.alert = () => {};
 
-// see https://gist.github.com/kristopherjohnson/5065599
-function readStdin(callback) {
-  var data = '';
-  var stdin = process.stdin;
-  stdin.resume();
-  stdin.setEncoding('utf8');
-  stdin.on('data', function (chunk) { data += chunk; });
-  stdin.on('end', function() {
-    callback(null, data);
-  });
-}
+let skip = true;
+let files = [];
+process.argv.forEach((filename) => {
+  if (filename === '--') {
+    skip = false;
+    return;
+  }
+  if (skip) {
+    return;
+  }
+  files.push(filename);
+});
 
-readStdin(function(err, res) {
-  var log = res.split('\n').filter(line => line.length > 0).map(line => JSON.parse(line));
-  analyzeLog(log).then(
-    result => {process.stdout.write(JSON.stringify(result))},
-    error => {console.error(error);}
-    );
+Promise.all(files.map(filename => {
+  let logRaw = fs.readFileSync(filename, 'utf-8');
+  let log = (
+    logRaw
+    .split('\n')
+    .filter(line => line.length > 0)
+    .map(line => JSON.parse(line)));
+  return analyzeLog(log).then(
+    result => ({filename, result}),
+    error => ({filename, error}),
+  );
+})).then(results => {
+  results.forEach(result => {process.stdout.write(JSON.stringify(result) + '\n')});
 });
--- src/textrec/analysis_util.py
+++ src/textrec/analysis_util.py
@@ -67,3 +67,75 @@ def get_log_analysis(participant, git_rev=None):
     analyzed = json.loads(result)
     analyzed['git_rev'] = git_rev
     return analyzed
+
+
+cheating_analysis_results = {}
+
+@mem.cache
+def get_raw_analysis_cheating(participant, logfile_size, git_rev, analysis_files):
+    """Abuse the joblib cache to store the analysis results."""
+    return cheating_analysis_results.get(participant, None)
+
+
+def get_log_analysis_many(participants):
+    cheating_analysis_results.clear()
+
+    # Load already-cached.
+    analysis_files = {
+        name: sha1_file(paths.frontend / name)
+        for name in ['src/Analyzer.js']
+    }
+
+    analyses = {}
+    todo = []
+    revisions_needed = set()
+    for participant in participants:
+        logpath = (paths.top_level / 'logs' / (participant+'.jsonl')).absolute()
+        git_rev = get_rev(logpath)
+        real_git_rev = rev_overrides.get(git_rev, git_rev)
+        logfile_size = os.path.getsize(logpath)
+
+        # See if we already have this analysis.
+        analysis_raw = get_raw_analysis_cheating(participant, logfile_size, real_git_rev, analysis_files)
+
+        if analysis_raw is None:
+            # need to compute it.
+            todo.append((participant, logpath, logfile_size, git_rev, real_git_rev))
+            revisions_needed.add((git_rev, real_git_rev))
+        else:
+            # Result was cached.
+            analyzed = json.loads(analysis_raw)
+            analyzed['git_rev'] = git_rev
+            analyses[participant] = analyzed
+
+    if todo:
+        logpaths = [x[1] for x in todo]
+        # Compute a batch of analyses.
+        for git_rev, real_git_rev in revisions_needed:
+            subprocess.check_call([paths.scripts / 'checkout-old.sh', git_rev, real_git_rev])
+        analyzer_path = str(paths.frontend / 'run-analysis')
+        completion = subprocess.run(
+            [analyzer_path, '--'] + logpaths,
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        for line in completion.stdout.split(b'\n'):
+            if len(line) == 0:
+                continue
+            line = json.loads(line.decode('utf-8'))
+            if 'error' in line:
+                print(f"Error processing {line['filename']}")
+            else:
+                result = line['result']
+                participant_id = result['participant_id']
+                print(f"Got result for {participant_id}")
+                analyses[participant_id] = result
+                # Store results as JSON because it's faster than pickle.
+                cheating_analysis_results[participant_id] = json.dumps(result)
+
+
+        for participant, logpath, logfile_size, git_rev, real_git_rev in todo:
+            # Store the analysis result.
+            get_raw_analysis_cheating.call(participant, logfile_size, real_git_rev, analysis_files)
+
+    cheating_analysis_results.clear()    
+    return analyses
