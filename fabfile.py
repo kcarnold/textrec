259042c40ece9b5cc9c0ee6e6dee3f794043e155
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2019-01-17 11:33:00 -0500

    Update fabric to fabric2.
    This was a lot more trouble than it was worth because of a Fabric bug.

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,7 @@
-from fabric.api import local, lcd, env, cd, run
-from fabric.contrib.project import rsync_project
+from fabric import task
+from patchwork.transfers import rsync
+
+# from fabric.contrib.project import rsync_project
 import subprocess
 import os
 import pathlib
@@ -7,41 +9,92 @@ import pathlib
 module_dir = pathlib.Path(__file__).resolve().parent
 assert os.getcwd() == str(module_dir)
 
-PATH_ON_REMOTE = '~/code/textrec'
-FRONTEND = 'src/frontend'
+PATH_ON_REMOTE = "~/code/textrec"
+FRONTEND = "src/frontend"
 FRONTEND_ON_REMOTE = os.path.join(PATH_ON_REMOTE, FRONTEND)
 
-env.use_ssh_config = True
-env.hosts = [
-   'gcp1',
-]
+# env.use_ssh_config = True
+my_hosts = ["textrec-aws"]
 
-def deploy():
-    local('git push')
-    git_rev = subprocess.check_output(['git', 'describe', '--always']).decode('utf-8').strip()
-    open(os.path.join(FRONTEND, '.env'), 'w').write(f'REACT_APP_GIT_REV={git_rev}\n')
-    rsync_project(remote_dir='~/code/textrec/models/', local_dir='models/', delete=True)
-    with cd('~/code/textrec'):
-        run('git pull')
-    with lcd(FRONTEND):
-        local('rm -rf src/old_versions/*')
-        local('npm run build')
-    rsync_project(remote_dir=os.path.join(FRONTEND_ON_REMOTE, 'build/'), local_dir=os.path.join(FRONTEND, 'build/'), delete=True)
-    # rsync -Pax models/ megacomplete-aws:/home/ubuntu/code/textrec/models/
-    with lcd(FRONTEND):
-        local(f'sentry-cli releases -o kenneth-arnold -p suggestionfrontend new {git_rev}')
-        local(f'sentry-cli releases -o kenneth-arnold -p suggestionfrontend files {git_rev} upload-sourcemaps src build')
+
+@task
+def git_push(c):
+    c.local("git push")
+
+
+def get_git_version():
+    return (
+        subprocess.check_output(["git", "describe", "--always"]).decode("utf-8").strip()
+    )
+
+
+@task
+def rsync_models(c):
+    rsync(
+        c, target="~/code/textrec/models/", source="models/", delete=False
+    )  # TODO: delete=True
+
+
+@task
+def git_pull(c):
+    c.run(f"cd {PATH_ON_REMOTE} && git pull")
+
+
+@task
+def build_frontend(c):
+    git_rev = get_git_version()
+    open(os.path.join(FRONTEND, ".env"), "w").write(f"REACT_APP_GIT_REV={git_rev}\n")
+
+    # This bug took me a long time to track down:
+    # - c.local runs the command in an EMPTY environment (see https://github.com/fabric/fabric/issues/1744)
+    # - which lacks a $PATH.
+    # - the npm script starts with a shebang, /usr/bin/env node.
+    # - env tries to look up 'node' in its path. Since PATH is empty, it falls back on /usr/bin:/bin.
+    # - So it fails with "env: node: No such file or directory".
+    # - But 'env -i sh' starts a shell, which provides a default PATH, which *does* include /usr/local/bin.
+    # - So "node" within that shell succeeds... but env node fails because that PATH is not *exported*.
+    # So we need to pass an explicit PATH. Boo.
+    c.local(f"cd {FRONTEND} && rm -rf src/old_versions/*")
+    c.local(f"cd {FRONTEND} && PATH=/usr/local/bin:/bin:/usr/bin npm run build")
+    rsync(
+        c,
+        target=os.path.join(FRONTEND_ON_REMOTE, "build/"),
+        source=os.path.join(FRONTEND, "build/"),
+        delete=False,  # TODO: delete=true
+    )
+    c.local(
+        f"cd {FRONTEND} && sentry-cli releases -o kenneth-arnold -p suggestionfrontend new {git_rev}"
+    )
+    c.local(
+        f"cd {FRONTEND} && sentry-cli releases -o kenneth-arnold -p suggestionfrontend files {git_rev} upload-sourcemaps src build"
+    )
+
+
+@task(hosts=my_hosts)
+def deploy(c):
+    git_push(c)
+    # rsync_models(c)
+    git_pull(c)
+    build_frontend(c)
+
 
 def get_data():
-    subprocess.run(['./scripts/pull-logs'], env=dict(os.environ, SERVER='gcp1'))
+    subprocess.run(["./scripts/pull-logs"], env=dict(os.environ, SERVER="gcp1"))
 
 
-def gen_traits():
+def gen_traits(c):
     def traits_flags(traits):
-        return ' '.join(f'--trait {trait}' for trait in traits)
-    NfC_E = traits_flags(['NFC', 'Extraversion'])
-    local(f'python -m textrec.gen_personality_inventory {NfC_E} --export-name traitData --out src/frontend/src/TraitData.js')
+        return " ".join(f"--trait {trait}" for trait in traits)
 
-    NfC_E_DT_O = traits_flags(['NFC', 'Extraversion', 'Trust', 'Openness'])
-    local(f'python -m textrec.gen_personality_inventory {NfC_E_DT_O} --export-name traitData --out src/frontend/src/TraitData_NfCEDTO.js')
-    local(f'python -m textrec.gen_personality_inventory {NfC_E_DT_O} --out data/trait_data.json')
+    NfC_E = traits_flags(["NFC", "Extraversion"])
+    c.local(
+        f"python -m textrec.gen_personality_inventory {NfC_E} --export-name traitData --out src/frontend/src/TraitData.js"
+    )
+    NfC_E_DT_O = traits_flags(["NFC", "Extraversion", "Trust", "Openness"])
+    c.local(
+        f"python -m textrec.gen_personality_inventory {NfC_E_DT_O} --export-name traitData --out src/frontend/src/TraitData_NfCEDTO.js"
+    )
+    c.local(
+        f"python -m textrec.gen_personality_inventory {NfC_E_DT_O} --out data/trait_data.json"
+    )
+
