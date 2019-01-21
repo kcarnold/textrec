b2d82effe94ef873cf77b40626ade71325410104
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2019-01-21 15:27:32 -0500

    Remove old log analysis code

--- src/textrec/analysis_util.py
+++ src/textrec/analysis_util.py
@@ -20,64 +20,13 @@ def get_rev(logpath):
     raise ValueError(f"No git revision logged in {logpath}")
 
 
-class AnalysisException(Exception):
-    def __init__(self, message):
-        super().__init__(message)
-        self.message = message
-
-
-@mem.cache
-def get_log_analysis_raw(logpath, logfile_size, git_rev, analysis_files=None):
-    # Ignore analysis_files; just use them to know when to invalidate the cache.
-    subprocess.check_call(
-        [
-            paths.scripts / "checkout-old.sh",
-            git_rev,
-            rev_overrides.get(git_rev, git_rev),
-        ]
-    )
-    analyzer_path = str(paths.frontend / "run-analysis")
-    with open(logpath) as logfile:
-        completion = subprocess.run(
-            [analyzer_path],
-            stdin=logfile,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-        )
-        if len(completion.stderr) != 0:
-            stderr = completion.stderr.decode("utf-8")
-            raise AnalysisException(stderr)
-        result = completion.stdout
-        assert len(result) > 0
-        return result
-
-
 def sha1_file(filename):
     with open(filename, "rb") as f:
         return hashlib.sha1(f.read()).hexdigest()
 
 
-def get_log_analysis(participant, git_rev=None):
-    analysis_files = {
-        name: sha1_file(paths.frontend / name)
-        for name in ["analyze.js", "run-analysis", "src/Analyzer.js"]
-    }
-    logpath = paths.top_level / "logs" / (participant + ".jsonl")
-    if git_rev is None:
-        git_rev = get_rev(logpath)
-    logfile_size = os.path.getsize(logpath)
-
-    try:
-        result = get_log_analysis_raw(
-            str(logpath), logfile_size, git_rev=git_rev, analysis_files=analysis_files
-        )
-    except AnalysisException as e:
-        raise AnalysisException(
-            f"Error while analyzing log for {participant}\n\n{e.message}\n\nThis was for participant={participant!r} git_rev={git_rev}"
-        )
-    analyzed = json.loads(result)
-    analyzed["git_rev"] = git_rev
-    return analyzed
+def get_log_analysis(participant):
+    return get_log_analysis_many([participant])[participant]
 
 
 cheating_analysis_results = {}
