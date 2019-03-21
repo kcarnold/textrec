9b869c28cb852b3736cd6fd95b91bbfb4d3193fc
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2019-03-21 15:22:46 -0400

    Revise script to count workers.

--- scripts/count_workers.py
+++ scripts/count_workers.py
@@ -3,9 +3,12 @@
 Find anyone who did the experiment or the annotation after already seeing the target images.
 """
 
-import pathlib
 import json
+import pathlib
+
+import pandas as pd
 import toolz
+
 from parse_questionformanswer import parse_qfa
 
 root = pathlib.Path("mturk-backup")
@@ -47,37 +50,37 @@ assignments = [
 ]
 
 # Summarize workers by subgroup.
-import pandas as pd
 assignments_df = pd.DataFrame(assignments)
 assignments_df["SubmitTime"] = pd.to_datetime(assignments_df['SubmitTime'])
 print(assignments_df.groupby('hit_type').WorkerId.nunique())
 
 
-assignments_by_worker = toolz.groupby("WorkerId", assignments)
-for worker_id, worker_assignments in assignments_by_worker.items():
-    prev_hits = []
-    for assignment in sorted(worker_assignments, key=lambda x: x["SubmitTime"]):
-        hit_title = id2hit[assignment["HITId"]]["Title"]
-        hit_type = titles[hit_title]
-        if hit_type == "write":
-            # if len(prev_hits) != 0:
-            #     print("")
-            prev_types = [typ for typ, hit in prev_hits]
-            if prev_types != []:
-                qfa = parse_qfa(assignment["Answer"])
-                print("Writing after", ",".join(prev_types), worker_id, qfa)
+def find_invalid_workers():
+    assignments_by_worker = toolz.groupby("WorkerId", assignments)
+    for worker_id, worker_assignments in assignments_by_worker.items():
+        prev_hits = []
+        for assignment in sorted(worker_assignments, key=lambda x: x["SubmitTime"]):
+            hit_title = id2hit[assignment["HITId"]]["Title"]
+            hit_type = titles[hit_title]
+            if hit_type == "write":
+                # if len(prev_hits) != 0:
+                #     print("")
+                prev_types = [typ for typ, hit in prev_hits]
+                if prev_types != []:
+                    qfa = parse_qfa(assignment["Answer"])
+                    print("Writing after", ",".join(prev_types), worker_id, qfa)
 
-        elif hit_type == "anno":
-            if not all(typ == "anno" for typ, hit in prev_hits):
-                print("Anno after writing:", worker_id)
-                break
-        
-        elif hit_type == "exploratory":
-            pass
+            elif hit_type == "anno":
+                if not all(typ == "anno" for typ, hit in prev_hits):
+                    print("Anno after writing:", worker_id)
+                    break
+            
+            elif hit_type == "exploratory":
+                pass
 
-        else:
-            print("UNKNOWN HIT TYPE", hit_type, hit_title)
-        prev_hits.append((hit_type, assignment))
+            else:
+                print("UNKNOWN HIT TYPE", hit_type, hit_title)
+            prev_hits.append((hit_type, assignment))
 
 
 def add_qualification(client):
@@ -90,4 +93,3 @@ if __name__ == "__main__":
     from mturk import MTurkClient
     client = MTurkClient(profile_name="iismturk", region_name="us-east-1")
     # add_qualification(client)
-
