860717a8c2e16f9ca1e1ff9207b0932d216fd843
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2019-03-21 15:20:46 -0400

    Revise scripts for managing participant data.

--- scripts/data_retention.py
+++ scripts/data_retention.py
@@ -7,63 +7,65 @@ import glob
 import json
 import datetime
 
+# TODO: actually use this to only remove old identifiers.
 oldest_time = datetime.datetime.now() - datetime.timedelta(days=30 * 6)
 
-all_csvs = subprocess.check_output(['mdfind', '-0', '-name', '.csv']).decode('utf8').split('\0')
+all_csvs = (
+    subprocess.check_output(["mdfind", "-0", "-name", ".csv"])
+    .decode("utf8")
+    .split("\0")
+)
 all_csvs.sort()
+
 
 def get_batch_results_csvs(candidate_csvs):
     all_batch_results = []
-    for fname in sorted(set(all_csvs)):
-        if not fname.endswith('.csv'):
-            print("Skipping", fname)
+    skips = []
+    errors = []
+    for fname in tqdm.tqdm(sorted(set(candidate_csvs)), desc="Finding MTurk CSVs"):
+        if 'backup_before_deidentification' in fname or not fname.endswith(".csv"):
+            skips.append(fname)
             continue
-        with open(fname, 'rb') as f:
-            for line in f:
-                if b'AssignmentId' in line and b'WorkerId' in line:
-                    all_batch_results.append(fname)
-                break
+        try:
+            with open(fname, "rb") as f:
+                for line in f:
+                    if b"AssignmentId" in line and b"WorkerId" in line:
+                        all_batch_results.append(fname)
+                    break
+        except OSError:
+            errors.append(fname)
+    if skips:
+        print("Skips:")
+        print('\n'.join(skips))
+    if errors:
+        print("Errors:")
+        print('\n'.join(errors))
     return all_batch_results
 
 
-backup_dir = pathlib.Path('backup_before_deidentification')
+backup_dir = pathlib.Path.home() / "backup_before_deidentification"
 
-def backup(files, backup_dir):
+
+def backup(fname, backup_dir):
     backup_dir.mkdir(exist_ok=True)
-
-    for fname in files:
-        assert '__' not in fname
-        backup_name = fname.replace('/', '__')
-        shutil.copy2(fname, backup_dir / backup_name)
+    assert "__" not in fname
+    backup_name = fname.replace("/", "__")
+    backup_path = backup_dir / backup_name
+    shutil.copy2(fname, backup_path)
+    return backup_path
 
 
 def delete_worker_id_column(files, backup_dir):
-    for fname in files:
-        backup_name = fname.replace('/', '__')
-        df = pd.read_csv(str(backup_dir / backup_name))
-        del df['WorkerId']
+    for fname in tqdm.tqdm(files, desc="Deleting worker ID column"):
+        backup_path = backup(fname, backup_dir)
+        df = pd.read_csv(str(backup_path))
+        del df["WorkerId"]
         df.to_csv(fname, index=False)
 
 
-
 logs = []
-for dir in 'suggestion textrec'.split():
-    logs.extend(glob.glob(f'/Users/kcarnold/code/{dir}/logs*/*.jsonl'))
-
-
-
-for fname in logs:
-    with open(fname) as f:
-#         print(fname)
-        line = None
-        for line in f:
-            break
-        if line is None:
-            continue
-        data = json.loads(line)
-        if 'platform_id' in data:
-            print(data['platform_id'])
-        
+for dir in "suggestion textrec".split():
+    logs.extend(glob.glob(f"/Users/kcarnold/thesis/{dir}/logs*/*.jsonl"))
 
 
 def deidentify_log(fname):
@@ -72,15 +74,21 @@ def deidentify_log(fname):
     if len(data) == 0:
         return
     login_event = data[0]
-    if login_event.get('platform_id') is not None:
-        pid = login_event['platform_id']
+    if login_event.get("platform_id") is not None:
+        pid = login_event["platform_id"]
         deidentified = pid[:4]
         print(pid, deidentified)
-        login_event['platform_id'] = deidentified
-        with open(fname, 'w') as f:
+        login_event["platform_id"] = deidentified
+        with open(fname, "w") as f:
             for event in data:
                 print(json.dumps(event), file=f)
 
-for fname in tqdm.tqdm_notebook(logs):
-    deidentify_log(fname)
 
+# TODO: Deidentify the MTurk backup too.
+
+if __name__ == '__main__':
+    all_batch_results = get_batch_results_csvs(all_csvs)
+    delete_worker_id_column(all_batch_results, backup_dir)
+
+    for fname in tqdm.tqdm(logs, desc="Deidentify logs"):
+        deidentify_log(fname)
--- scripts/find_invalid_workers.py
+++ scripts/find_invalid_workers.py
@@ -8,58 +8,86 @@ import json
 import toolz
 from parse_questionformanswer import parse_qfa
 
-root = pathlib.Path('mturk-backup')
-hits = [json.load(open(f)) for f in root.glob('*/meta.json')]
+root = pathlib.Path("mturk-backup")
+hits = [json.load(open(f)) for f in root.glob("*/meta.json")]
 
-hits_by_title = toolz.groupby('Title', hits)
+hits_by_title = toolz.groupby("Title", hits)
 
 titles = {
-    'Which image fits a given description?': 'anno',
-    'Pick the image that this sentence is more likely to be describing.': 'anno',
-    'Which images fit a given description?': 'anno',
-    'Try writing with different mobile-phone keyboards': 'write',
-    'Try typing with different mobile-phone keyboards': 'write,'
+    "Which image fits a given description?": "anno",
+    "Pick the image that this sentence is more likely to be describing.": "anno",
+    "Which images fit a given description?": "anno",
+    "Which caption is more specific?": "anno",
+    "Try writing with different mobile-phone keyboards": "write",
+    "Try typing with different mobile-phone keyboards": "write",
+    "Write a restaurant review": "write",
+    "Write a movie review": "write",
+    "Find images that people not in a certain group would misunderstand": "exploratory"
 }
 
-relevant_hits = [hit for hit in hits if hit['Title'] in titles.keys()]
-irrelevant_hits = {hit['Title'] for hit in hits if hit['Title'] not in titles}
+relevant_hits = [hit for hit in hits if hit["Title"] in titles.keys()]
+irrelevant_hits = {hit["Title"] for hit in hits if hit["Title"] not in titles}
 if irrelevant_hits:
     print()
     print("Skipping other HITS:")
     print("\n".join(irrelevant_hits))
     print()
-id2hit = {hit['HITId']: hit for hit in hits}
+id2hit = {hit["HITId"]: hit for hit in hits}
+
+def lookup_type(hit_id):
+    hit_title = id2hit[hit_id]["Title"]
+    hit_type = titles[hit_title]
+    return dict(hit_title=hit_title, hit_type=hit_type)
+
 
 assignments = [
-    assignment
+    dict(assignment, **lookup_type(assignment["HITId"]))
     for hit in relevant_hits
-    for assignment in json.load(open(root / hit['HITId'] / 'assignments.json'))]
+    for assignment in json.load(open(root / hit["HITId"] / "assignments.json"))
+]
 
-assignments_by_worker = toolz.groupby('WorkerId', assignments)
+# Summarize workers by subgroup.
+import pandas as pd
+assignments_df = pd.DataFrame(assignments)
+assignments_df["SubmitTime"] = pd.to_datetime(assignments_df['SubmitTime'])
+print(assignments_df.groupby('hit_type').WorkerId.nunique())
+
+
+assignments_by_worker = toolz.groupby("WorkerId", assignments)
 for worker_id, worker_assignments in assignments_by_worker.items():
     prev_hits = []
-    for assignment in sorted(worker_assignments, key=lambda x: x['SubmitTime']):
-        hit_type = titles[id2hit[assignment['HITId']]['Title']]
-        if hit_type == 'write':
+    for assignment in sorted(worker_assignments, key=lambda x: x["SubmitTime"]):
+        hit_title = id2hit[assignment["HITId"]]["Title"]
+        hit_type = titles[hit_title]
+        if hit_type == "write":
             # if len(prev_hits) != 0:
             #     print("")
             prev_types = [typ for typ, hit in prev_hits]
             if prev_types != []:
-                qfa = parse_qfa(assignment['Answer'])
-                print("Writing after", ','.join(prev_types), worker_id, qfa)
+                qfa = parse_qfa(assignment["Answer"])
+                print("Writing after", ",".join(prev_types), worker_id, qfa)
 
-        else:
-            assert hit_type == 'anno'
-            if not all(typ == 'anno' for typ, hit in prev_hits):
+        elif hit_type == "anno":
+            if not all(typ == "anno" for typ, hit in prev_hits):
                 print("Anno after writing:", worker_id)
                 break
+        
+        elif hit_type == "exploratory":
+            pass
+
+        else:
+            print("UNKNOWN HIT TYPE", hit_type, hit_title)
         prev_hits.append((hit_type, assignment))
 
 
 def add_qualification(client):
-    client.qualify_workers(client.get_qualification_id('did-captioning'), assignments_by_worker.keys())
+    client.qualify_workers(
+        client.get_qualification_id("did-captioning"), assignments_by_worker.keys()
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     from mturk import MTurkClient
-    client = MTurkClient(profile_name='iismturk', region_name='us-east-1')
-    add_qualification(client)
+    client = MTurkClient(profile_name="iismturk", region_name="us-east-1")
+    # add_qualification(client)
+
