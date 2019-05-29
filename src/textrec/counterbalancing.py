cb748b9ee1fb21e93701e0e8358321f789f72788
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2019-05-29 17:01:17 -0400

    add counterbalance

--- src/textrec/counterbalancing.py
+++ src/textrec/counterbalancing.py
@@ -37,6 +37,9 @@ BATCH_DATA = {
     "idea3r1": dict(
         n_conditions=3, config="idea"
     ), # Actually enable cues.
+    "idea3r2": dict(
+        n_conditions=3, config="idea"
+    ), # Word cues!
 }
 
 invalid = set("h52x67 3vf5fg 73qq5q ffhgxm mhh838 j39263 pqf6q5 49cm8f".split())
