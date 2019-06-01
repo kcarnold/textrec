ad236da33c1acf879f432198c98d18d5fd24929a
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2019-06-01 15:02:30 -0400

    Use dotenv to allow overriding dataset path.

--- src/textrec/paths.py
+++ src/textrec/paths.py
@@ -1,29 +1,30 @@
+import os
 import pathlib
 
 
 class paths:
     module_dir = pathlib.Path(__file__).resolve().parent
     top_level = module_dir.parent.parent
-    frontend = top_level / 'src' / 'frontend'
-    ui = frontend / 'build'
-    logdir = top_level / 'logs'
-    data = top_level / 'data'
-    analyzed = data / 'analyzed'
-    gruntwork = data / 'gruntwork'
+    frontend = top_level / "src" / "frontend"
+    ui = frontend / "build"
+    logdir = top_level / "logs"
+    data = top_level / "data"
+    analyzed = data / "analyzed"
+    gruntwork = data / "gruntwork"
 
-    figures = top_level / 'reports' / 'figures'
+    figures = top_level / "reports" / "figures"
 
-    cache = top_level / 'cache'
-    models = top_level / 'models'
-    scripts = top_level / 'scripts'
+    cache = top_level / "cache"
+    models = top_level / "models"
+    scripts = top_level / "scripts"
 
     imgdata_h5_subset = models / "feats_by_imgid.subset.h5"
-    imgdata_h5_all = top_level / 'models-local' / 'feats_by_imgid.h5'
+    imgdata_h5_all = top_level / "models-local" / "feats_by_imgid.h5"
     imgdata_h5 = imgdata_h5_subset
 
-    old_code = frontend / 'src' / 'old_versions'
+    old_code = frontend / "src" / "old_versions"
 
-    vendor = top_level.parent / 'vendor'
-    kenlm_bin = vendor / 'kenlm' / 'build' / 'bin'
+    vendor = top_level.parent / "vendor"
+    kenlm_bin = vendor / "kenlm" / "build" / "bin"
 
-    dataset_root = pathlib.Path('/Data')
+    dataset_root = pathlib.Path(os.environ.get("DATASET_ROOT", "/Data"))
