fc09e2810234b2eeaa1321185a1b0adad0e2804d
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2019-06-03 13:41:36 -0400

    .

--- scripts/get_category_membership.py
+++ scripts/get_category_membership.py
@@ -1,13 +1,21 @@
+import glob
 import gzip
 import json
-import glob
+
+import pandas as pd
 import tqdm
 
 results = []
-for path in tqdm.tqdm(glob.glob('stream*.jsonl.gz')):
+for path in tqdm.tqdm(glob.glob("stream*.jsonl.gz")):
     with gzip.open(path) as f:
-        for line in tqdm.tqdm(f):
+        for line in tqdm.tqdm(f, mininterval=5):
             article = json.loads(line)
-            del article['content']
+            del article["content"]
             results.append(article)
 
+category_memberships = []
+for article in results:
+    for category_name in article["infoboxes"]:
+        category_memberships.append((article["name"], category_name))
+
+category_memberships = pd.DataFrame(category_memberships)
