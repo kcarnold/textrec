1c638dbec2c1cbe21f3b05a0c032c7e67944da1e
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2019-06-04 12:23:43 -0400

    Make it possible to manually rerun failed relevance array tasks

--- scripts/collect_relevance_data.sh
+++ scripts/collect_relevance_data.sh
@@ -3,16 +3,18 @@
 #SBATCH -p serial_requeue
 #SBATCH -n 1 # one core
 #SBATCH -N 1 # on one node
-#SBATCH -t 0-6:00
+#SBATCH -t 0-12:00
 #SBATCH --mem 16000
 #SBATCH -o collect_relevance_%A_%a.out
 #SBATCH -e collect_relevance_%A_%a.err
 
 PYTHON=~/.cache/pypoetry/virtualenvs/textrec-py3.6/bin/python
 
-TOTAL=${SLURM_ARRAY_TASK_COUNT}
+DATASET=${DATASET:-yelp}
+TOTAL=${TOTAL:-$SLURM_ARRAY_TASK_COUNT}
+CUR=${CUR:-$SLURM_ARRAY_TASK_ID}
 
-BATCH_DATA=${SLURM_ARRAY_TASK_ID}/${SLURM_ARRAY_TASK_COUNT}
+BATCH_DATA=${CUR}/${TOTAL}
 echo Batch $BATCH_DATA >&2
 
-"${PYTHON}" scripts/example_eval_collect.py yelp 50:250:5 --subset=$BATCH_DATA
+"${PYTHON}" scripts/example_eval_collect.py "$DATASET" 50:250:5 --subset=$BATCH_DATA
