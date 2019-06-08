8cb3b61c4dc4191dc9cc368d5203b21515d4901c
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2019-06-08 15:59:46 -0400

    Quiet down some debug logs

--- src/textrec/runserver.py
+++ src/textrec/runserver.py
@@ -2,11 +2,15 @@ from textrec.paths import paths
 import logging
 
 logging.basicConfig(
-    format='%(asctime)s: %(levelname)s: %(name)s: %(message)s',
+    format="%(asctime)s: %(levelname)s: %(name)s: %(message)s",
     level=logging.DEBUG,
     handlers=[
         logging.FileHandler(str(paths.logdir / "server.log")),
-        logging.StreamHandler()
-    ])
+        logging.StreamHandler(),
+    ],
+)
+logging.getLogger("matplotlib").setLevel(logging.INFO)
+
 from textrec.app import main
+
 main()
