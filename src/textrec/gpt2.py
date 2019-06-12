4d97d64ce444edf73307a8ebb205895ccac91a5b
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2019-06-12 11:25:34 -0400

    Monkey-patch to fix a GPT2 tokenization bug.

--- src/textrec/gpt2.py
+++ src/textrec/gpt2.py
@@ -1,13 +1,23 @@
-import numpy as np
+import regex as re
 import torch
-import torch.nn.functional as F
-from tqdm import tqdm
 
 from pytorch_pretrained_bert import GPT2LMHeadModel, GPT2Tokenizer
 
 enc = GPT2Tokenizer.from_pretrained("gpt2")
 model = GPT2LMHeadModel.from_pretrained("gpt2")
 model.eval()
+
+# Monkey-patch to fix https://github.com/huggingface/pytorch-pretrained-BERT/issues/537
+def tokenize(self, text):
+    """ Tokenize a string. """
+    bpe_tokens = []
+    for token in re.findall(self.pat, text):
+        token = "".join(self.byte_encoder[b] for b in token.encode("utf-8"))
+        bpe_tokens.extend(bpe_token for bpe_token in self.bpe(token).split(" "))
+    return bpe_tokens
+
+
+GPT2Tokenizer.tokenize = tokenize
 
 
 def nll(phrase, prefix=". "):
