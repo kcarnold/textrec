716682bc9409b9c5cddf4fbeacadb5e3564cdad4
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2019-06-12 11:28:00 -0400

    Add a batch process to compute GPT2 NLL.

---
+++ src/textrec/analysis_step3.py
@@ -0,0 +1,42 @@
+"""
+Add automated measures.
+"""
+import json
+import pathlib
+import pickle
+
+import numpy as np
+
+from textrec.paths import paths
+from textrec.util import mem
+from textrec import gpt2
+
+cached_gpt2_nll = mem.cache(gpt2.nll)
+
+
+def main(batch):
+    # We compute NLL on lowercased text because capitalization was inconsistent.
+    
+    batch_path: pathlib.Path = paths.data / "analyzed" / "idea" / batch
+    with open(batch_path / "step1.pkl", "rb") as f:
+        analyses = pickle.load(f)
+
+    trials = analyses["trial_level"]
+    trials["nll_total_lower"] = trials.finalText.apply(
+        lambda txt: cached_gpt2_nll(txt.strip().lower())
+    )
+    trials['nll_per_char'] = trials.nll_total_lower / trials.finalText.str.len()
+
+    with open(batch_path / "step3.pkl", "wb") as f:
+        pickle.dump(analyses, f)
+
+    return analyses
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("batch")
+    opts = parser.parse_args()
+    globals().update(main(opts.batch))
