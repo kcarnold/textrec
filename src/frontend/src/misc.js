b03d2b862c64023bb553c6ee3631c668940ee36b
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2019-06-24 09:52:52 -0400

    Revert "Factor finalDataLogger. Going to revert."
    This reverts commit b280e3f656e8b440dd48a2e9d53bf91acdbf5766.

--- src/frontend/src/misc.js
+++ src/frontend/src/misc.js
@@ -23,34 +23,30 @@ export function getDemoConditionName(clientId: string): ?string {
   return null;
 }
 
-const defaultFinalDataHandler = state => {
-  let trialData = [];
-  state.experiments.forEach((expState, expName) => {
-    trialData.push({
-      name: expName,
-      ...expState.getSerialized(),
-    });
-  });
-  let finalData = {
-    screenTimes: state.screenTimes.map(screen => ({
-      ...screen,
-      name: state.screens[screen.num].screen,
-    })),
-    controlledInputs: [...state.controlledInputs.toJS()],
-    trialData,
-  };
-  return finalData;
-};
-
-export const finalDataLogger = (state, callback = defaultFinalDataHandler) => {
+export const finalDataLogger = state => {
   state.eventHandlers.push((state, event) => {
     if (event.type === "next") {
       let delta = event.delta || 1;
       if (delta === 1 && state.screenNum === state.screens.length - 1) {
+        let trialData = [];
+        state.experiments.forEach((expState, expName) => {
+          trialData.push({
+            name: expName,
+            ...expState.getSerialized(),
+          });
+        });
+        let finalData = {
+          screenTimes: state.screenTimes.map(screen => ({
+            ...screen,
+            name: state.screens[screen.num].screen,
+          })),
+          controlledInputs: [...state.controlledInputs.toJS()],
+          trialData,
+        };
         return [
           {
             type: "finalData",
-            finalData: callback(state),
+            finalData,
           },
         ];
       }
